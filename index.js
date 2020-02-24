468a5f9d7b2048591f9f485278793828f7019457
Gopal Anand <32189444+gopalanand333@users.noreply.github.com>
2020-02-24 17:34:20 +0530

    number generator.js

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @param {ISODateString} date 
+ */
+const generateId= (date)=>{
+        const numericArray = new Array();
+        var temp = 0;
+        date.split("T")[0].split("-").forEach(ele=>numericArray.push(parseInt(ele)));// split the dateTime string into parts and sum them
+        date.split("T")[1].split(":").forEach(ele=>numericArray.push(parseFloat(ele)));
+        var id =  numericArray.reduce(reducer);
+       // log the new generated id, substr can be used to change the length of numberic ID
+        console.log(id.toString().replace(".","").substr(1,6));
+   
+    
+};
+
+/**
+ * 
+ * @param {number} accumulator - each element of the array
+ * @param {number} currentValue - any additional vallue to be added
+ */
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+generateId(new Date().toISOString());
